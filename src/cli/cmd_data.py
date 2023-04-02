26eae8c8098565304785cb9f76632c8d42aca2be
LEE URBANEK <lugithub@gmail.com>
2023-04-01 20:20:55 -0400

    initial commit

--- src/cli/cmd_data.py
+++ src/cli/cmd_data.py
@@ -50,6 +50,86 @@ def _add_ohlc_table(ctx_obj, db_con):
 # 	SubmissionDate TIMESTAMP);'''
 # cursor.execute(createTable)
 
+# =======
+
+# eem = [{'date': '2023-03-09T00:00:00.000Z', 'close': 38.04, 'high': 38.58, 'low': 37.96, 'open': 38.51, 'volume': 40118857, 'adjClose': 38.04, 'adjHigh': 38.58, 'adjLow': 37.96, 'adjOpen': 38.51, 'adjVolume': 40118857, 'divCash': 0.0, 'splitFactor': 1.0}, {'date': '2023-03-10T00:00:00.000Z', 'close': 37.84, 'high': 38.25, 'low': 37.8, 'open': 38.02, 'volume': 49316671, 'adjClose': 37.84, 'adjHigh': 38.25, 'adjLow': 37.8, 'adjOpen': 38.02, 'adjVolume': 49316671, 'divCash': 0.0, 'splitFactor': 1.0}]
+# iwm = [{'date': '2023-03-09T00:00:00.000Z', 'close': 181.41, 'high': 187.27, 'low': 181.28, 'open': 186.73, 'volume': 33546890, 'adjClose': 181.41, 'adjHigh': 187.27, 'adjLow': 181.28, 'adjOpen': 186.73, 'adjVolume': 33546890, 'divCash': 0.0, 'splitFactor': 1.0}, {'date': '2023-03-10T00:00:00.000Z', 'close': 176.18, 'high': 180.39, 'low': 174.255, 'open': 180.39, 'volume': 67388021, 'adjClose': 176.18, 'adjHigh': 180.39, 'adjLow': 174.255, 'adjOpen': 180.39, 'adjVolume': 67388021, 'divCash': 0.0, 'splitFactor': 1.0}]
+
+# =======
+
+# connector.execute("insert into DATAGERMANY values (NULL,?,?,?,?,?)", *row)
+
+# =======
+
+# import datetime
+# import sqlite3
+
+# # get the current datetime and store it in a variable
+# currentDateTime = datetime.datetime.now()
+
+# # make the database connection with detect_types
+# connection = sqlite3.connect(
+#     'StudentAssignment.db',
+#     detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+# )
+# cursor = connection.cursor()
+
+# # create table in database
+# createTable = '''CREATE TABLE ASSIGNMENT (
+# 	StudentId INTEGER,
+# 	StudentName VARCHAR(100),
+# 	SubmissionDate TIMESTAMP);'''
+# cursor.execute(createTable)
+
+# # create query to insert the data
+# insertQuery = """INSERT INTO ASSIGNMENT
+# 	VALUES (?, ?, ?);"""
+
+# # insert the data into table
+# cursor.execute(insertQuery, (1, "Virat Kohli",
+# 							currentDateTime))
+# cursor.execute(insertQuery, (2, "Rohit Pathak",
+# 							currentDateTime))
+# print("Data Inserted Successfully !")
+
+# # commit the changes,
+# # close the cursor and database connection
+# connection.commit()
+# cursor.close()
+# connection.close()
+
+# =======
+
+# import datetime
+# import sqlite3
+
+# make a database connection and cursor object
+# connection = sqlite3.connect(
+#     'StudentAssignment.db',
+#     detect_types=sqlite3.PARSE_DECLTYPES | sqlite3.PARSE_COLNAMES
+# )
+# cursor = connection.cursor()
+
+# # select query to retrieve data
+# cursor.execute("SELECT * from ASSIGNMENT where StudentId = 2")
+# fetchedData = cursor.fetchall()
+
+# # to access specific fetched data
+# for row in fetchedData:
+# 	StudentID = row[0]
+# 	StudentName = row[1]
+# 	SubmissionDate = row[2]
+# 	print(StudentName, ", ID -",
+# 		StudentID, "Submitted Assignments")
+# 	print("Date and Time : ",
+# 		SubmissionDate)
+# 	print("Submission date type is",
+# 		type(SubmissionDate))
+
+# # commit the changes,
+# # close the cursor and database connection
+# cursor.close()
+# connection.close()
 
 @click.command('data', short_help="Fetch online OHLC price and volume data", help="""
 \b
